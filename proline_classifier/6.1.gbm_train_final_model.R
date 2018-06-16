e5048392a667fd0e7e0d1386ffa21688c8dd0da0
Xiyao Long <xlong2@jhu.edu>
2018-06-15 22:30:01 -0400

    untrack files

--- proline_classifier/6.1.gbm_train_final_model.R
+++
@@ -1,6 +0,0 @@
-# script for training the final model after finding the best hyperparameter. 
-args <- commandArgs(trailingOnly = TRUE)
-print(args)
-getwd()
-source("0.load_function_and_data.R")
-execute_training_rscript_final_model(args)
